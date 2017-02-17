1c9b4bff235718c0953cb4d292b8fdfebf80181d
Dzianis <dzianis.makeichyk@gmail.com>
2017-02-17 22:58:52 +0100

    project

--- my-app/src/Pages/PortfolioGrid/PortfolioGrid.jsx
+++ my-app/src/Pages/PortfolioGrid/PortfolioGrid.jsx
@@ -5,6 +5,7 @@ import isMediaLessThan from '../../web_modules/isMediaLessThan';
 import routeMap from '../../../routeMap.json';
 import { Motion, spring } from 'react-motion';
 import classnames from 'classnames';
+import Forest from '../Elements/icon_svg';
 
 export default class PortfolioGrid extends React.Component {
 
@@ -64,38 +65,39 @@ export default class PortfolioGrid extends React.Component {
     if (this.props.currentProjectName !== null && this.props.currentProjectName !== this.props.project.name) {
       visible = false;
     }
-    return (
-        <li
-            className="portfolio-item"
-            onMouseOver={this.handleMouseOver.bind(this)}
-            onMouseOut={this.handleMouseOut.bind(this)}
-            style={visible?{}:{visibility:'hidden'}}
-        >
-          <Link
-              to={routeMap.project_details.replace(':slug', this.props.project.name)}
-              className={classnames('animation-link', {'is-mobile':this.context.currentMedia <= 'Mobile' })}
-          >
-            {isMediaLessThan('Small', this.context.currentMedia) && (
-                <div className={classnames({'portfolio-info-box':this.context.currentMedia <= 'Mobile' })}>
-                  <h6 className="portfolio-title qanelas-bold">
-                    {this.props.project.title}
-                  </h6>
-                  <img className="portfolio-img" src={'img/project_grid/' + this.props.project.background + '.png'} alt={this.props.project.name}/>
-                  <p className="project-mini-categories">
-                    {this.props.project.short_description}
-                  </p>
-                </div>
-            )}
-            {isMediaGreaterThan('Mobile', this.context.currentMedia) && (
-                <div>
-                  {DescriptionOnHover}
-                  {DescriptionOutHover}
-                </div>
-            )}
-          </Link>
-        </li>
-    );
-
     console.log(this.state.width);
+
+    return (
+      <li
+        className="portfolio-item"
+        onMouseOver={this.handleMouseOver.bind(this)}
+        onMouseOut={this.handleMouseOut.bind(this)}
+        style={visible?{}:{visibility:'hidden'}}
+      >
+        <Link
+          to={routeMap.project_details.replace(':slug', this.props.project.name)}
+          className={classnames('animation-link', {'is-mobile':this.context.currentMedia <= 'Mobile' })}
+        >
+          {isMediaLessThan('Small', this.context.currentMedia) && (
+            <div className={classnames({'portfolio-info-box':this.context.currentMedia <= 'Mobile' })}>
+              <h6 className="portfolio-title qanelas-bold">
+                {this.props.project.title}
+              </h6>
+              <p className="project-mini-categories">
+                {this.props.project.short_description}
+              </p>
+              <img className="portfolio-img" src={'img/project_grid/' + this.props.project.background + '.png'} alt={this.props.project.name}/>
+              <Forest />
+            </div>
+          )}
+          {isMediaGreaterThan('Mobile', this.context.currentMedia) && (
+            <div>
+              {DescriptionOnHover}
+              {DescriptionOutHover}
+            </div>
+          )}
+        </Link>
+      </li>
+    );
   }
 }
