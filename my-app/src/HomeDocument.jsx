9732f0b74923bd4964c6622d329215ed7dd5aa5a
Dzianis <dzianis.makeichyk@gmail.com>
2017-03-01 12:41:35 +0100

    IE functions

--- my-app/src/HomeDocument.jsx
+++ my-app/src/HomeDocument.jsx
@@ -5,6 +5,7 @@ import Menu from './Menu'
 import routeMap from '../routeMap.json';
 import MediaDetectElement from './web_modules/MediaDetectElement';
 import AudioWolf from './Pages/Elements/audioIcon'
+import platform from 'platform'
 
 class Home extends Component {
 
@@ -87,80 +88,97 @@ class Home extends Component {
         // console.log(location.pathname !== routeMap.home);
       var things = ['rock', 'fun', 'more work'];
       const thing = things[Math.floor(Math.random()*things.length)];
-      console.log('The computer :' + thing);
-      console.log(this.props.location.pathname);
+      const isLoggedIn = (platform.name === 'Microsoft Edge' || 'IE');
+      console.log(platform.name);
+      console.log(isLoggedIn);
+
+
       return (
         <div className="App">
-            <header className="header-wrap">
+          {isLoggedIn ? (
+            <p>hello</p>
+          ) : (
+            <div>
+              <header className="header-wrap">
                 <nav>
-                    <div className="header-container">
-                        <div className="pull-left">
-                            <Link
-                              to="/"
-                              className={classNameLogo}>Dzianis Makeichyk
-                            </Link>
-                          <span onClick={this.onPause}>
-                            <AudioWolf />
+                  <div className="header-container">
+                    <div className="pull-left">
+                      <Link
+                        to="/"
+                        className={classNameLogo}>Dzianis Makeichyk
+                      </Link>
+                      <span onClick={this.onPause}>
+            <AudioWolf />
+            </span>
+                    </div>
+                    <div className="pull-right">
+                      <div
+                        ref="myMenu"
+                        onClick={this.handleClick}
+                        className="burger-click-region"
+                      >
+                        <div className="menu-burger">
+                          <span
+                            ref="topLine"
+                            className={classNamePiece}
+                          >
+
+                          </span>
+                          <span
+                            ref="middleLine"
+                            className={classNamePiece}
+                          >
+
+                          </span>
+                          <span
+                            ref="bottomLine"
+                            className={classNamePiece}
+                          >
+
+                          </span>
+                          <span
+                            ref="otherLine"
+                            className={classNameOther}
+                          >
+
                           </span>
                         </div>
-                        <div className="pull-right">
-                            <div
-                              ref="myMenu"
-                              onClick={this.handleClick}
-                              className="burger-click-region"
-                            >
-                                <div className="menu-burger">
-                                    <span
-                                      ref="topLine"
-                                      className={classNamePiece}
-                                    ></span>
-                                    <span
-                                      ref="middleLine"
-                                      className={classNamePiece}
-                                    ></span>
-                                    <span
-                                      ref="bottomLine"
-                                      className={classNamePiece}
-                                    ></span>
-                                    <span
-                                      ref="otherLine"
-                                      className={classNameOther}
-                                    ></span>
-                                </div>
-                            </div>
-                        </div>
+                      </div>
                     </div>
-                    {(this.state.menuVisible &&
-                        <Menu
-                          routeMap={routeMap}
-                          location={this.props.location}
-                          onToggle={this.handleClick.bind(this)}
-                        />
-                    )}
+                  </div>
+                  {(this.state.menuVisible &&
+                    <Menu
+                      routeMap={routeMap}
+                      location={this.props.location}
+                      onToggle={this.handleClick.bind(this)}
+                    />
+                  )}
                 </nav>
-            </header>
-            <main>
-              {this.props.children}
-              {(this.props.location.pathname &&
-                <div className="preloader">
-                  <p className="preloader-text qanelas-bold">
-                    Pencil Sharpener
-                    <svg version="1.1" x="0" y="0" viewBox="0 0 357 357" enableBackground="new 0 0 357 357" xmlSpace="preserve">
-                      <g>
-                        <g id="remove">
-                          <path d="M357,204H0v-51h357V204z" fill="#FFFFFF"/>
+              </header>
+              <main>
+                {this.props.children}
+                {(this.props.location.pathname &&
+                  <div className="preloader">
+                    <p className="preloader-text qanelas-bold">
+                      Pencil Sharpener
+                      <svg version="1.1" x="0" y="0" viewBox="0 0 357 357" enableBackground="new 0 0 357 357" xmlSpace="preserve">
+                        <g>
+                          <g id="remove">
+                            <path d="M357,204H0v-51h357V204z" fill="#FFFFFF"/>
+                          </g>
                         </g>
-                      </g>
-                    </svg>
-                    it's
-                    <span className="preloader-color-text">
+                      </svg>
+                      it's
+                      <span className="preloader-color-text">
                       &nbsp;{thing}
                     </span>
-                  </p>
-                </div>
-              )}
+                    </p>
+                  </div>
+                )}
                 <MediaDetectElement onMediaChange={this.onMediaChange} />
-            </main>
+              </main>
+            </div>
+          )}
         </div>
         );
     }
