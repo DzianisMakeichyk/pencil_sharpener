10319f5a5bcdb647466d4714ceb20064cc086891
Dzianis <dzianis.makeichyk@gmail.com>
2017-05-03 11:13:56 +0200

    add en index

--- my-app/src/pages/Contact/components/Submit.js
+++ my-app/src/pages/Contact/components/Submit.js
@@ -4,14 +4,12 @@ var { intlShape } = require('react-intl');
 
 var SubmitElement = React.createClass({
   render: function() {
-    const languesLocation = this.context.intl.locale;
     var done = this.props.percent >= 100;
     var classes = classSet({
       'form-submit': true,
       'form-submit-disabled': !done
     });
     var label = done ? this.context.intl.messages.now_yes : this.context.intl.messages.nope;
-    languesLocation
     return (
       <div className="form-group">
         <button type="submit" className={classes}>{label}</button>
